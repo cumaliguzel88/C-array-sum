f39fe2c99d5daec513360fd4c280c5620e93c6cf
cumaliguzel88 <128859533+cumaliguzel88@users.noreply.github.com>
2023-03-25 01:20:20 +0300

    COK BOYUTLU DİZİLER İLE TOPLAMA
    C# İLE COK BOYUTLU DİZİLER İLE TOPLAMA

---
+++ DİZİLER ÖRENKLER/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DİZİLER_ÖRENKLER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 3;
+            int asalsayilar;
+
+
+            int[,] dizi = new int[y, x];
+            dizi[0, 0] = 10;
+            dizi[0, 1] = 20;
+            dizi[1, 0] = 30;
+            dizi[1, 1] = 40;
+            dizi[2, 0] = 50;
+            dizi[2, 1] = 60;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(" {0} ", dizi[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
